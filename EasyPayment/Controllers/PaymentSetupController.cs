fc8e8e21d6b0562d840e1549d37c31bfce0da04a
mos6ke <mos6ke@gmail.com>
2020-11-13 22:23:33 +0100

    [Moses]PaymentRequestModel

--- EasyPayment/Controllers/PaymentSetupController.cs
+++ EasyPayment/Controllers/PaymentSetupController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace EasyPayment.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentSetupController : ControllerBase
+    public class PaymentSetupController : BaseController
     {
     }
 }
